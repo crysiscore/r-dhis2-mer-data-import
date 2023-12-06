3cc7d8e4aa59904d7379e93e7cb90b16c2d1117f
Agnaldo Samuel <agnaldosamuel3@gmail.com>
2023-12-06 10:28:05 +0200

    new reporting periods

--- templates_generator.R
+++ templates_generator.R
@@ -1,11 +1,22 @@
 # Generate config files for Monthly Uploads
-# 1 - dataset_templates/datimDataSetElementsCC.RData
-load("~/Git/ccs_datim_maping/dataset_templates/datimDataSetElementsCC.RData")
+# How to handle changes if mer indicators or org_units change
+
+#Change here
+working_dir <- '/Users/asamuel/Projects/ccs-datim-data-import'
+
+# 1 - DHIS2 CCS (e-analysis) 
+# Se houver mudancas nos indicadores do DHIS2 CCS (e-analysis) deve-se fazer os devidos mapeamentos nos ficheiros correspondentes aos datasets
+# ('MER ATS COMMUNITY.xlsx',  'MER CARE & TREATMENT.xlsx'  , 'MER PREVENTION.xlsx','MER ATS.xlsx' ,'MER HEALTH SYSTEM.xlsx'  ,   'MER SMI.xlsx' ,  'NON MER MAPPING MDS.xlsx')
+# e gerar novo template
+
+# - dataset_templates/datimDataSetElementsCC.RData
+load(paste0(working_dir, '/dataset_templates/datimDataSetElementsCC.RData' ))
+
 df_datim_indicators <- df_datim_indicators[0,]
 df_datim_indicators$indicator <-""
 
 
-setwd(dir = '~/Git/ccs_datim_maping/mapping/')
+setwd(dir = paste0(working_dir, '/mapping/'))
 vec_files <- c('MER ATS COMMUNITY.xlsx',  'MER CARE & TREATMENT.xlsx'  , 'MER PREVENTION.xlsx','MER ATS.xlsx' ,
                'MER HEALTH SYSTEM.xlsx'  ,   'MER SMI.xlsx' ,  'NON MER MAPPING MDS.xlsx')
 
@@ -28,17 +39,23 @@ for (file  in vec_files) {
 
 }
 
-save(df_datim_indicators,file = '../dataset_templates/datimDataSetElementsCC.RData')
+save(df_datim_indicators,file = paste0(working_dir, '/dataset_templates/datimDataSetElementsCC.RData'))
+
+
+
+# 2 -  PEPFAR DATIM 
+# Se houver mudancas no DATIM  deve-se baixar o novo data dictionary do datim que inclui as mudancas nos data elements, identificar o indicador que sofreu allteracao e efectuar as mudancas
+# ('MER ATS COMMUNITY.xlsx',  'MER CARE & TREATMENT.xlsx'  , 'MER PREVENTION.xlsx','MER ATS.xlsx' ,'MER HEALTH SYSTEM.xlsx'  ,   'MER SMI.xlsx' ,  'NON MER MAPPING MDS.xlsx')
+# e geral novo template 
 
 # Gerar apartir dos ficheiros mapeados exclusivamente para o formualrio DATIM
 # Generate config files for Datim Uploadds
-# 1 - dataset_templates/datimMappingTemplate.RData
-load("~/Git/ccs_datim_maping/dataset_templates/datimDataSetElementsCC.RData")
+# dataset_templates/datimMappingTemplate.RData
+load( file = paste0(working_dir, "/dataset_templates/datimDataSetElementsCC.RData"))
 datim_mapping_template <- df_datim_indicators[0,]
 datim_mapping_template$indicator <-""
 
-
-setwd(dir = '~/Git/ccs_datim_maping/mapping/Datim/')
+setwd(dir =paste0(working_dir, '/mapping/Datim/'))
 vec_files <- c('MER ATS COMMUNITY.xlsx',  'MER CARE & TREATMENT.xlsx'  , 'MER PREVENTION.xlsx','MER ATS.xlsx' ,
                'MER HEALTH SYSTEM.xlsx'  ,   'MER SMI.xlsx'  )
 
@@ -52,6 +69,9 @@ for (file  in vec_files) {
       
     } else {
       df_tmp <- readxl::read_xlsx(path = file,sheet = sheet,skip = 1,col_names = TRUE)
+      if(ncol(df_tmp)>11){
+        df_tmp <- df_tmp [,c(1:11)]
+      }
       df_tmp$indicator <-  gsub(" ","",sheet)
       assign(x = gsub(" ","",sheet) ,value = df_tmp,envir = .GlobalEnv  )
       
@@ -60,34 +80,41 @@ for (file  in vec_files) {
   }
   
 }
-setwd(dir = '~/Git/ccs_datim_maping/dataset_templates')
+setwd(dir = paste0(working_dir, '/dataset_templates'))
 save(datim_mapping_template,file = 'datimMappingTemplate.RData')
 
 
-# 2 - Templates : Dataset_templates/dataset_templates.RDATA
-# Para cada dataset novo deve-se buscar o template e agregar no dataframe dataset_templates.RDATA
-#
+# 3 - NOVO DATASET DHIS CCS (e-analysis) 
+# Sempre que se cria um novo formulario no DHIS e se deseja importar dados atraves do API deve-se:
 
+# 3.1  Para cada dataset/form  novo buscar o template e agregar no dataframe dataset_templates.RDATA
+# Configurar os parametros do dataset a seguir
 api_dhis_base_url <- "http://192.168.1.10:5400"
 api_dhis_datasets <- 'https://mail.ccsaude.org.mz:5459/api/dataSets/'
 dataset_id_mer_datim          <- "Z9agMHXo792"
-dataset_id_non_mer            <- "LUsbbPX9hlO"
+dataset_id_non_mer            <- "LUsbbPX9hlO" # novo
 
-load(file ='/home/agnaldo/Git/ccs_datim_maping/dataset_templates/dataset_templates.RDATA' )
-#datavalueset_template_dhis2_datim         <- getDhis2DatavalueSetTemplate(url.api.dhis.datasets = api_dhis_datasets, dataset.id = dataset_id_mer_datim)
-# Novo dataset
-df_non_mer_mds <-   getDhis2DatavalueSetTemplate(url.api.dhis.datasets = api_dhis_datasets, dataset.id = dataset_id_non_mer)
+
+# Uncomment the following lines
+# load(file = paste0(working_dir, '/dataset_templates/dataset_templates.RDATA' ))
+# NOT RUN ( Ja foi adicionado)
+# datavalueset_template_dhis2_datim         <- getDhis2DatavalueSetTemplate(url.api.dhis.datasets = api_dhis_datasets, dataset.id = dataset_id_mer_datim)
+# NOT RUN ( Ja foi adicionado)
+# df_non_mer_mds <-   getDhis2DatavalueSetTemplate(url.api.dhis.datasets = api_dhis_datasets, dataset.id = dataset_id_non_mer) # Novo dataset
+
+
 # Junta o novo data set com o template padrao
-datavalueset_template_dhis2_datim = plyr::rbind.fill(datavalueset_template_dhis2_datim,df_non_mer_mds)
-save(datavalueset_template_dhis2_datim, file = '/home/agnaldo/Git/ccs_datim_maping/dataset_templates/dataset_templates.RDATA')
+# datavalueset_template_dhis2_datim = plyr::rbind.fill(datavalueset_template_dhis2_datim,df_non_mer_mds)
+# save(datavalueset_template_dhis2_datim, file =  paste0(working_dir, '/dataset_templates/dataset_templates.RDATA'))
      
-     
-template_dhis2_datim <- datavalueset_template_dhis2_datim
-save(template_dhis2_datim, file = 'dataset_templates/datim_dataelement_ids.RDATA')
+#template_dhis2_datim <- datavalueset_template_dhis2_datim
+#save(template_dhis2_datim, file = paste0(working_dir, '/dataset_templates/datim_dataelement_ids.RDATA'))
 
-# 
-# 3- If  orgunits are changed then re-generate the template
-df_ccs_data_exchange_orgunits <- readxl::read_xlsx(path = 'mapping/CCS DATA EXCHANGE ORG UNITS.xlsx' ,col_names = TRUE)
-save(df_ccs_data_exchange_orgunits , file = 'dataset_templates/ccsDataExchangeOrgUnits.RData')
-# 
-# 
+# 4 - NEW ORG UNITS
+# If  orgunits are changed then re-generate the template
+#df_ccs_data_exchange_orgunits <- readxl::read_xlsx(path = paste0(working_dir, '/mapping/CCS DATA EXCHANGE ORG UNITS.xlsx' ,col_names = TRUE))
+#save(df_ccs_data_exchange_orgunits , file = paste0(working_dir, '/dataset_templates/ccsDataExchangeOrgUnits.RData'))
+ 
+ 
+library(devtools)
+install_git('https://github.com/pepfar-datim/datim-validation')
